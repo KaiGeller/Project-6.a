665356249b24ed97e3f521cfa3a553c1cbdd3749
KaiGeller <gellerka@oregonstate.edu>
2022-05-04 21:07:58 -0700

    1

---
+++ find_median.py
@@ -0,0 +1,12 @@
+#Kai Geller
+#GitHub User: KaiGeller
+#5/4/2022
+#To find the median of a given list of numbers
+def find_median(some_nums):
+    """This function will return the median value of a list"""
+    if len(some_nums) % 2 != 0:
+        sorted(some_nums)
+        print(sorted(some_nums)[len(some_nums)//2])
+    if len(some_nums) % 2 == 0:
+        sorted(some_nums)
+        print((sorted(some_nums)[len(some_nums)//2+1]+sorted(some_nums)[len(some_nums)//2])/2)
